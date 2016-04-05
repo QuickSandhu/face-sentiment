bb2091857c2d442ff9aff71eae80c6ca38adedbc
QuickSandhu <raymonsandhu@yahoo.com>
2016-04-05 10:48:48 -0700

    Sorted the sentiment ratings file

--- convnet.py
+++ convnet.py
@@ -17,7 +17,6 @@ from PIL import Image
 import csv
 PIXELS = 256
 CHANNELS = 1
-image_list = []
 img_dir = '/home/rsandhu/face-sentiment/face-sentiment/jaffe/'
 img_names = os.listdir(img_dir)
 img_names.sort()
@@ -29,10 +28,10 @@ with open('/home/rsandhu/face-sentiment/face-sentiment/sentiment-ratings-sorted.
         rowdata = [float(rowdata[1]), float(rowdata[2]), float(rowdata[3]), float(rowdata[4]),float(rowdata[5]), float(rowdata[6])]
         Y.append(rowdata)
 
-Y = np.asarray(Y)/5
+Y = np.asarray(Y) / 5 # Jaffe images rated out of 5
 
 
-
+# create an empty matrix of zeros that will hold input images for training
 X = np.zeros((img_names.__len__(), CHANNELS, PIXELS, PIXELS), dtype='float32')
 i = 0
 for filename in img_names:
@@ -44,9 +43,9 @@ for filename in img_names:
 #################
 #   The Model   #
 #################
-model = Sequential()
 # input: 256 x 256 images with 1 channel
 # this applies 32 convolution filters of size 3 x 3 each
+model = Sequential()
 model.add(Convolution2D(32,3,3, border_mode='valid', input_shape=(1,256,256)))
 model.add(Activation('relu'))
 model.add(Convolution2D(32,3,3))
@@ -66,6 +65,15 @@ model.add(Activation('softmax'))
 sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd)
 
-model.fit(X, Y, batch_size=32, nb_epoch=3)
 
+
+#################
+#   Training    #
+#################
+print("training model...")
+hist = model.fit(X, Y,
+          batch_size = 10,
+          nb_epoch = 200,
+          )
+print(hist.history)
 loss, accuracy = model.evaluate(X, Y, show_accuracy=True, verbose=True)
