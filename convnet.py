bf18b1fb3e1b78915e1994d61e2023f69f9c9643
QuickSandhu <raymonsandhu@yahoo.com>
2016-04-06 10:43:29 -0700

    Just NN things

--- convnet.py
+++ convnet.py
@@ -8,6 +8,7 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD
 from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import np_utils
 
 ##############################
 #  Machine Dependendent Vars #
@@ -15,15 +16,22 @@ from keras.preprocessing.image import ImageDataGenerator
 img_dir = '/Users/sraymon/face-sentiment/jaffe/'
 ratings_path = '/Users/sraymon/face-sentiment/sentiment-ratings-sorted.csv'
 
+
+
 ###########
 #  Vars   #
 ###########
 PIXELS = 256
 CHANNELS = 1
-data_augmentation=True
-nb_epoch = 10
-batch_size = 16
-valid_size = 21
+data_augmentation=False
+nb_epoch = 2000
+batch_size = 15
+valid_size = 20
+valid_size_percent = 0.1
+
+print("epochs: {}, batch-size: {}, validation-set-size: {} ".format(nb_epoch, batch_size, valid_size))
+
+
 
 #################
 #   Load Data   #
@@ -33,30 +41,31 @@ import csv
 img_names = os.listdir(img_dir)
 img_names.sort()
 
-Y = []
-with open(ratings_path, 'r') as file:
-    reader = csv.reader(file, delimiter=' ')
-    for rowdata in reader:
-        rowdata = [float(rowdata[1]), float(rowdata[2]), float(rowdata[3]), float(rowdata[4]),float(rowdata[5]), float(rowdata[6])]
-        Y.append(rowdata)
-
-Y = np.asarray(Y) / 5 # Jaffe images rated out of 5
-# convert to probabilities
-i = 0
-for row in Y:
-    Y[i,] = Y[i,] / sum(Y[i,])
-    i += 1
+sentiment_dict = {'AN' : 0,
+                  'DI' : 1,
+                  'SU' : 2,
+                  'FE' : 3,
+                  'HA' : 4,
+                  'NE' : 5,
+                  'SA' : 6}
 
 # create an empty matrix of zeros that will hold input images for training
 X = np.zeros((img_names.__len__(), CHANNELS, PIXELS, PIXELS), dtype='float32')
+Y = np.zeros((img_names.__len__(),1), dtype='uint8')
 i = 0
 for filename in img_names:
     im = Image.open(img_dir + filename)
-    X[i] = np.asarray(im, dtype='float32') / 255
+    X[i] = np.asarray(im, dtype='float32')
+    sentiment = filename[3:5]
+    Y[i,0] = sentiment_dict[sentiment]
     i += 1
 
+X /= 255.0
+Y = np_utils.to_categorical(Y)
 Y_test, Y_train = Y[:valid_size,:], Y[valid_size:,:]
 X_test, X_train = X[:valid_size,:], X[valid_size:,:]
+
+
 
 #################
 #   The Model   #
@@ -65,26 +74,42 @@ X_test, X_train = X[:valid_size,:], X[valid_size:,:]
 # this applies 32 convolution filters of size 3 x 3 each
 
 model = Sequential()
-model.add(Convolution2D(32,3,3, border_mode='valid', input_shape=(1,256,256)))
+model.add(MaxPooling2D(pool_size=(2,2), input_shape=(1,256,256), border_mode='same'))
+
+model.add(Convolution2D(32,3,3, border_mode='same', init='lecun_uniform'))
 model.add(Activation('relu'))
 model.add(Convolution2D(32,3,3))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
 
-model.add(Convolution2D(32,3,3, border_mode='valid'))
+model.add(Convolution2D(32,3,3, border_mode='same'))
 model.add(Activation('relu'))
-model.add(Convolution2D(32,3,3, border_mode='valid'))
+model.add(Convolution2D(32,3,3, border_mode='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(3,3)))
+model.add(Dropout(0.25))
+
+model.add(Convolution2D(32,3,3, border_mode='same', init='lecun_uniform'))
+model.add(Activation('relu'))
+model.add(Convolution2D(32,3,3))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
 
+model.add(Convolution2D(32,3,3, border_mode='same'))
+model.add(Activation('relu'))
+model.add(Convolution2D(32,3,3, border_mode='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(3,3)))
+model.add(Dropout(0.25))
+
 model.add(Flatten())
-model.add(Dense(256))
+model.add(Dense(400))
 model.add(Activation('relu'))
 model.add(Dropout(0.25))
 
-model.add(Dense(6))
+model.add(Dense(7))
 model.add(Activation('softmax'))
 
 sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
@@ -105,21 +130,23 @@ if data_augmentation:
         height_shift_range=0.1,
         horizontal_flip=True,
         vertical_flip=False)
-    datagen.fit(X)
+    datagen.fit(X_train)
 
-    hist = model.fit_generator(datagen.flow(X, Y, batch_size=batch_size),
-                        samples_per_epoch=X_train.shape[0],
-                        nb_epoch=nb_epoch,
-                        show_accuracy=True,
-                        validation_data=(X_test, Y_test)
-                        )
+    hist = model.fit_generator(datagen.flow(X_train, Y_train, batch_size=batch_size),
+                                samples_per_epoch=X_train.shape[0],
+                                nb_epoch=nb_epoch,
+                                show_accuracy=True,
+                                validation_data=(X_test, Y_test)
+                                )
     print(hist.history)
 else:
     print("training model...")
     hist = model.fit(X, Y,
-                     batch_size = 10,
-                     nb_epoch = 1,
+                     batch_size = batch_size,
+                     nb_epoch = nb_epoch,
+                     validation_split=valid_size_percent,
                      show_accuracy=True,
+                     shuffle=True, verbose=2
                      )
     print(hist.history)
-    loss, accuracy = model.evaluate(X, Y, show_accuracy=True, verbose=True)
+    # loss, accuracy = model.evaluate(X, Y, show_accuracy=True, verbose=True)
